58c000c6640dbdf4f3cc661af4f31c48bd9b0bdf
Zakk <muhammadzakaria616@gmail.com>
2022-12-21 16:30:23 +0500

    added dynamic data

---
+++ data.js
@@ -0,0 +1,101 @@
+var data = [
+    {
+        id: '1',
+        stepTitle: 'Idea Generation',
+        assignedTo: 'Farhan Hashmi',
+        status: 'done',
+        cardClass: 'card-done',
+        cardHeaderClass: 'card-header-done',
+        icon: 'check_circle',
+        iconClass: 'icons-done',
+        descClass: 'description-done'
+    },
+    {
+        id: '2',
+        stepTitle: 'Idea Selection',
+        assignedTo: 'Salman Anwer Malik',
+        status: 'done',
+        cardClass: 'card-done',
+        cardHeaderClass: 'card-header-done',
+        icon: 'check_circle',
+        iconClass: 'icons-done',
+        descClass: 'description-done'
+    },
+    {
+        id: '3',
+        stepTitle: 'Idea Analysis',
+        assignedTo: 'Nida Kazmi',
+        status: 'done',
+        cardClass: 'card-done',
+        cardHeaderClass: 'card-header-done',
+        icon: 'check_circle',
+        iconClass: 'icons-done',
+        descClass: 'description-done'
+    },
+    {
+        id: '4',
+        stepTitle: 'Idea Analysis',
+        assignedTo: 'Nida Kazmi',
+        status: 'done',
+        cardClass: 'card-done',
+        cardHeaderClass: 'card-header-done',
+        icon: 'check_circle',
+        iconClass: 'icons-done',
+        descClass: 'description-done'
+    },
+    {
+        id: '5',
+        stepTitle: 'Idea Analysis',
+        assignedTo: 'Nida Kazmi',
+        status: 'done',
+        cardClass: 'card-done',
+        cardHeaderClass: 'card-header-done',
+        icon: 'check_circle',
+        iconClass: 'icons-done',
+        descClass: 'description-done'
+    },
+    {
+        id: '6',
+        stepTitle: 'Idea Analysis',
+        assignedTo: 'Nida Kazmi',
+        status: 'done',
+        cardClass: 'card-done',
+        cardHeaderClass: 'card-header-done',
+        icon: 'check_circle',
+        iconClass: 'icons-done',
+        descClass: 'description-done'
+    },
+    {
+        id: '7',
+        stepTitle: 'Idea Analysis',
+        assignedTo: 'Nida Kazmi',
+        status: 'done',
+        cardClass: 'card-done',
+        cardHeaderClass: 'card-header-done',
+        icon: 'check_circle',
+        iconClass: 'icons-done',
+        descClass: 'description-done'
+    },
+    {
+        id: '8',
+        stepTitle: 'Idea Analysis',
+        assignedTo: 'Nida Kazmi',
+        status: 'done',
+        cardClass: 'card-done',
+        cardHeaderClass: 'card-header-done',
+        icon: 'check_circle',
+        iconClass: 'icons-done',
+        descClass: 'description-done'
+    },
+    {
+        id: '9',
+        stepTitle: 'Idea Presentation',
+        assignedTo: 'Adil Javed',
+        status: 'pending',
+        cardClass: 'card-pending',
+        cardHeaderClass: 'card-header-pending',
+        icon: 'info',
+        iconClass: 'icons-pending',
+        descClass: 'description-pending'
+    }
+]
